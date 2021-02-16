b1462e496cf9d5b2ee3f50f783c77a4519722e8a
Julien Prugne <julien@webeleon.dev>
2021-02-16 10:16:56 +0100

    initial repo setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# unofficial API client for dev.to API
+
+[dev.to API documentation](https://docs.dev.to/api/)
---
+++ src/main.test.ts
@@ -0,0 +1,7 @@
+import { DevTo } from './main';
+
+describe('main export', () => {
+  it('should export the DevTo client', () => {
+    expect(DevTo).toBeDefined();
+  });
+});
